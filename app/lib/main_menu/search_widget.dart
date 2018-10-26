481ad85028d4846bab47e421c4a95b1a6e15ef71
Umberto Sonnino <rimher@gmail.com>
2018-10-26 18:47:07 +0200

    Improvements to the search widget
    Moved from a button to a TextFIeld, and now is Stateful so that it can have a cancel button when focused

--- app/lib/main_menu/search_widget.dart
+++ app/lib/main_menu/search_widget.dart
@@ -2,38 +2,82 @@ import 'package:flutter/material.dart';
 
 import "../colors.dart";
 
-class SearchWidget extends StatelessWidget
+class SearchWidget extends StatefulWidget
 {
+    @override
+    State<StatefulWidget> createState() => _SearchState();
+}
+
+class _SearchState extends State<SearchWidget>
+{
+    final FocusNode searchFocus = FocusNode();
+    bool _isSearching = false;
+
+    @override
+    initState()
+    {
+        super.initState();
+        searchFocus.addListener(()
+        {
+            setState(
+                (){
+                    _isSearching = searchFocus.hasFocus;
+                }
+            );
+        });
+    }
+
+    @override
+    void dispose() 
+    {
+        searchFocus.dispose();
+        super.dispose();
+    }
+
     @override
     Widget build(BuildContext context) 
     {
-        return FlatButton(
-                    onPressed: () => print("TAP"), // TODO: implement search view.
-                    color: lightGrey,
-                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-                    child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: 
-                        [
-                            Container(
-                                margin: EdgeInsets.only(right:15.5),
-                                child: Image.asset(
-                                        "assets/search_icon.png",
-                                        height:17.5,
-                                        width: 17.5,
-                                        color: Colors.black.withOpacity(0.5)
-                                    ),
-                                ),
-                                Text(
-                                    "Type to search...",
-                                    style: TextStyle(
+        return Container(
+            decoration: BoxDecoration(
+                color: lightGrey,
+                borderRadius: BorderRadius.circular(20.0)
+            ),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: 
+                [
+                    Expanded(
+                        child: Theme(
+                            data: ThemeData(
+                                primaryColor: darkText.withOpacity(darkText.opacity*0.5),
+                            ),
+                            child: TextField(
+                                focusNode: searchFocus,
+                                decoration: new InputDecoration(
+                                    hintText: "Search",
+                                    hintStyle: TextStyle(
+                                        height: 18.0/16.0, // Set line to 18
                                         fontSize: 16.0,
                                         fontFamily: "Roboto",
                                         color: darkText.withOpacity(darkText.opacity*0.5),
                                     ),
-                                )
-                        ]
+                                    prefixIcon: Icon(Icons.search),
+                                    suffixIcon: searchFocus.hasFocus ? IconButton(
+                                        icon: Icon(Icons.cancel),
+                                        onPressed: () => searchFocus.unfocus(),
+                                        ) : null,
+                                    border: InputBorder.none
+                                ),
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: "Roboto",
+                                    color: darkText.withOpacity(darkText.opacity),
+                                ),
+                            ),
+                        ),
                     )
-                );
+                ]
+            )
+        );
     }
 }

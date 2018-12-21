43328ea3c677904e6460e38f66915b2581b85508
Umberto Sonnino <rimher@gmail.com>
2018-12-21 16:40:22 +0100

    ThumbnailDetailWidget Documentation

--- app/lib/main_menu/thumbnail_detail_widget.dart
+++ app/lib/main_menu/thumbnail_detail_widget.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/cupertino.dart';
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:timeline/colors.dart';
+import 'package:timeline/timeline/timeline_entry.dart';
 
-import "thumbnail.dart";
-import "package:timeline/colors.dart";
-import "package:timeline/timeline/timeline_entry.dart";
+import 'thumbnail.dart';
 
+/// Define a custom function for the callback that's passed into this widget by the [MainMenuWidget]. 
+/// 
+/// This callback allows the [MainMenuWidget] to display the [TimelineWidget] and position it
+/// to the right start/end time for the [entry].
 typedef TapSearchResultCallback(TimelineEntry entry);
 
+/// This widget lays out nicely the [timelineEntry] provided.
+/// 
+/// It shows the [ThumbnailWidget] for that entry on the left, and the label with the date of the entry
+/// on its immediate right.
+/// 
+/// This widget is used while displaying the search results in the [MainMenuWidget], and in the
+/// [FavoritesPage] widget.
 class ThumbnailDetailWidget extends StatelessWidget {
   final TimelineEntry timelineEntry;
+  /// Whether to show a divider line on the bottom of this widget. Defaults to `true`.
   final bool hasDivider;
+  /// Callback to navigate to the timeline (see [MainMenuWidget._tapSearchResult()]).
   final TapSearchResultCallback tapSearchResult;
 
   ThumbnailDetailWidget(this.timelineEntry,
       {this.hasDivider = true, this.tapSearchResult, Key key})
       : super(key: key);
 
+
+  /// Use [Material] & [InkWell] to show a Material Design ripple effect on the row.
+  /// [InkWell] provides also a callback for custom onTap behavior.
+  /// 
+  /// The widget is laid out with a [Column] that lays out the contents of the entry, and the divider,
+  /// and a [Row], which contains the [ThumbnailWidget], and the entry information.
   @override
   Widget build(BuildContext context) {
-    // Use (Material + InkWell) to show a ripple effect on the row.
     return Material(
         color: Colors.transparent,
         child: InkWell(

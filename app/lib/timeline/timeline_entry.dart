32d5ab743d2adf24f8f2993d7ec9cffaf7902f56
Umberto Sonnino <rimher@gmail.com>
2018-12-24 11:21:47 +0100

    timeline_entry Documentation

--- app/lib/timeline/timeline_entry.dart
+++ app/lib/timeline/timeline_entry.dart
@@ -1,43 +1,18 @@
-import "dart:ui" as ui;
+import 'dart:ui' as ui;
 import 'dart:ui';
-import "package:nima/nima.dart" as nima;
-import "package:nima/nima/animation/actor_animation.dart" as nima;
-import "package:nima/nima/math/aabb.dart" as nima;
-import "package:flare/flare.dart" as flare;
-import "package:flare/flare/animation/actor_animation.dart" as flare;
-import "package:flare/flare/math/aabb.dart" as flare;
-import "package:flare/flare/math/vec2d.dart" as flare;
 
-class TimelineImage extends TimelineAsset {
-  ui.Image image;
-}
+import 'package:flare/flare.dart' as flare;
+import 'package:flare/flare/animation/actor_animation.dart' as flare;
+import 'package:flare/flare/math/aabb.dart' as flare;
+import 'package:flare/flare/math/vec2d.dart' as flare;
+import 'package:nima/nima.dart' as nima;
+import 'package:nima/nima/animation/actor_animation.dart' as nima;
+import 'package:nima/nima/math/aabb.dart' as nima;
 
-class TimelineAnimatedAsset extends TimelineAsset {
-  double animationTime = 0.0;
-  bool loop;
-  double offset = 0.0;
-  double gap = 0.0;
-}
-
-class TimelineNima extends TimelineAnimatedAsset {
-  nima.FlutterActor actorStatic;
-  nima.FlutterActor actor;
-  nima.ActorAnimation animation;
-  nima.AABB setupAABB;
-}
-
-class TimelineFlare extends TimelineAnimatedAsset {
-  flare.FlutterActorArtboard actorStatic;
-  flare.FlutterActorArtboard actor;
-  flare.ActorAnimation animation;
-
-  flare.ActorAnimation intro;
-  flare.ActorAnimation idle;
-  List<flare.ActorAnimation> idleAnimations;
-
-  flare.AABB setupAABB;
-}
-
+/// An object representing the renderable assets loaded from `timeline.json`.
+/// 
+/// Each [TimelineAsset] encapsulates all the relevant properties for drawing,
+/// as well as maintaining a reference to its original [TimelineEntry].
 class TimelineAsset {
   double width;
   double height;
@@ -50,39 +25,78 @@ class TimelineAsset {
   TimelineEntry entry;
 }
 
+/// A renderable image.
+class TimelineImage extends TimelineAsset {
+  ui.Image image;
+}
+
+/// This asset also has information regarding its animations.
+class TimelineAnimatedAsset extends TimelineAsset {
+  bool loop;
+  double animationTime = 0.0;
+  double offset = 0.0;
+  double gap = 0.0;
+}
+
+/// An `Nima` Asset.
+class TimelineNima extends TimelineAnimatedAsset {
+  nima.FlutterActor actorStatic;
+  nima.FlutterActor actor;
+  nima.ActorAnimation animation;
+  nima.AABB setupAABB;
+}
+
+/// A `Flare` Asset.
+class TimelineFlare extends TimelineAnimatedAsset {
+  flare.FlutterActorArtboard actorStatic;
+  flare.FlutterActorArtboard actor;
+  flare.ActorAnimation animation;
+
+  /// Some Flare assets will have multiple idle animations (e.g. 'Humans'),
+  /// others will have an intro&idle animation (e.g. 'Sun is Born'). 
+  /// All this information is in `timeline.json` file, and it's de-serialized in the
+  /// [Timeline.loadFromBundle()] method, called during startup.
+  /// and custom-computed AABB bounds to properly position them in the timeline.
+  flare.ActorAnimation intro;
+  flare.ActorAnimation idle;
+  List<flare.ActorAnimation> idleAnimations;
+  flare.AABB setupAABB;
+
+}
+
+/// A label for [TimelineEntry].
 enum TimelineEntryType { Era, Incident }
 
+/// Each entry in the timeline is represented by an instance of this object.
+/// Each favorite, search result and detail page will grab the information from a reference
+/// to this object.
+/// 
+/// They are all initialized at startup time by the [BlocProvider] constructor.
 class TimelineEntry {
   TimelineEntryType type;
-  double start;
-  double end;
-  String _label;
-  int lineCount = 1;
-  String get label => _label;
-  set label(String value) {
-    _label = value;
-    int start = 0;
-    lineCount = 1;
-    while (true) {
-      start = _label.indexOf("\n", start);
-      if (start == -1) {
-        break;
-      }
-      lineCount++;
-      start++;
-    }
-  }
 
+  /// Used to calculate how many lines to draw for the bubble in the timeline.
+  int lineCount = 1;
+  /// 
+  String _label;
   String articleFilename;
   String id;
 
   Color accent;
 
+  /// Each entry constitues an element of a tree: 
+  /// eras are grouped into spanning eras and events are placed into the eras they belong to.
   TimelineEntry parent;
   List<TimelineEntry> children;
+  /// All the timeline entries are also linked together to easily access the next/previous event.
+  /// After a couple of seconds of inactivity on the timeline, a previous/next entry button will appear
+  /// to allow the user to navigate faster between adjacent events.
   TimelineEntry next;
   TimelineEntry previous;
 
+  /// All these parameters are used by the [Timeline] object to properly position the current entry.
+  double start;
+  double end;
   double y = 0.0;
   double endY = 0.0;
   double length = 0.0;
@@ -98,12 +112,30 @@ class TimelineEntry {
   double favoriteY = 0.0;
   bool isFavoriteOccluded = false;
 
+  TimelineAsset asset;
+
   bool get isVisible {
     return opacity > 0.0;
   }
 
-  TimelineAsset asset;
+  String get label => _label;
+  /// Some labels already have newline characters to adjust their alignment.
+  /// Detect the occurrence and add information regarding the line-count.
+  set label(String value) {
+    _label = value;
+    int start = 0;
+    lineCount = 1;
+    while (true) {
+      start = _label.indexOf("\n", start);
+      if (start == -1) {
+        break;
+      }
+      lineCount++;
+      start++;
+    }
+  }
 
+  /// Pretty-printing for the entry date.
   String formatYearsAgo() {
     if (start > 0) {
       return start.round().toString();
@@ -111,11 +143,13 @@ class TimelineEntry {
     return TimelineEntry.formatYears(start) + " Ago";
   }
 
+  /// Debug information.
   @override
   String toString() {
     return "TIMELINE ENTRY: $label -($start,$end)";
   }
 
+  /// Helper method.
   static String formatYears(double start) {
     String label;
     int valueAbs = start.round().abs();
